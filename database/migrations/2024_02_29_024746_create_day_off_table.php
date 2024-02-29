c2537605ef3cec581bc2f86063791e5acd1467ce
João Pedro Ennes <joaopedroennes@outlook.com>
2024-02-28 23:51:23 -0300

    feat: add day off table

---
+++ database/migrations/2024_02_29_024746_create_day_off_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('day_off', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->nullable();
+            $table->date('date');
+            $table->enum('type', ['vacation', 'sick', 'personal', 'holiday', 'other'])->default('other');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('day_off');
+    }
+};
