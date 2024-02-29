19075c18cbb877121dde5addb70efdf4ea74ff2a
João Pedro Ennes <joaopedroennes@outlook.com>
2024-02-28 23:38:42 -0300

    feat: mount widget with db data

--- app/Filament/Widgets/HoursPerDayChart.php
+++ app/Filament/Widgets/HoursPerDayChart.php
@@ -13,19 +13,48 @@ class HoursPerDayChart extends ChartWidget
     {
         $punches = PunchResource::getEloquentQuery()
             ->where('user_id', auth()->id())
-            ->where('date', '<', now()->format('Y-m-d'))
-            ->where('date', '>=', now()->subDays(6)->format('Y-m-d'))
+            ->where('date', '<=', now()->format('Y-m-d'))
+            ->where('date', '>', now()->subDays(6)->format('Y-m-d'))
             ->where('approved', true)
             ->get();
+
+        $widgetDays = [];
+        $dataPerDay = [];
+        $widgetData = [];
+
+        foreach ($punches as $punch) {
+            $date = new \DateTime($punch->date);
+            $day = $date->format('D');;
+
+            if (!in_array($day, $widgetDays)) {
+                $widgetDays[] = $day;
+            }
+
+            $dataPerDay[$day][] = $punch->time;
+        }
+
+        foreach ($dataPerDay as $day => $punches) {
+            $totalHours = 0.0;
+            $punchesCount = count($punches);
+
+            for ($i = 0; $i < $punchesCount; $i += 2) {
+                $start = new \DateTime($punches[$i]);
+                $end = new \DateTime($punches[$i + 1]);
+
+                $totalHours += round(($start->diff($end)->h * 60 + $start->diff($end)->i) / 60, 2);
+            }
+
+            $widgetData[] = $totalHours;
+        }
 
         return [
             'datasets' => [
                 [
                     'label' => 'Hours',
-                    'data' => [0, 9, 7.5, 6.9, 7.9, 8, 0],
+                    'data' => $widgetData,
                 ],
             ],
-            'labels' => ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+            'labels' => $widgetDays,
         ];
     }
 
