368e1286355ed2bc1c104a7010feacfd57972553
João Ennes <joaopedroennes@outlook.com>
2024-03-04 11:56:32 -0300

    refactor: create new files to centralize the code and reduce duplicates

--- app/Infolists/Components/Section.php
+++ app/Infolists/Components/Section.php
@@ -2,55 +2,12 @@
 
 namespace App\Infolists\Components;
 
+use App\Concerns\CanBeSection;
 use Filament\Infolists\Components\Component;
 
 class Section extends Component
 {
-    protected string $view = 'infolists.components.section';
+    use CanBeSection;
 
-    protected string | \Closure | null $description = null;
-    protected string | \Closure | null $icon = null;
-
-    public function __construct(
-        protected string | \Closure $heading,
-    )
-    {
-
-    }
-
-    public static function make(string | \Closure $heading): static
-    {
-        return app(static::class, [
-            'heading' => $heading,
-        ]);
-    }
-
-    public function description(string | \Closure | null $description): static
-    {
-        $this->description = $description;
-
-        return $this;
-    }
-
-    public function icon(string | \Closure | null $icon): static
-    {
-        $this->icon = $icon;
-
-        return $this;
-    }
-
-    public function getHeading(): string
-    {
-        return $this->evaluate($this->heading);
-    }
-
-    public function getDescription(): ?string
-    {
-        return $this->evaluate($this->description);
-    }
-
-    public function getIcon(): ?string
-    {
-        return $this->evaluate($this->icon);
-    }
+    protected string $view = 'section';
 }
